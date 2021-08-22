cfc119f9f1603fe7f7f84e16730b85f6ec3434fa
Gent Spahiu <gentsp@moosend.com>
2021-08-23 00:56:02 +0200

    Add gallery

--- js/main.js
+++ js/main.js
@@ -4,18 +4,26 @@ $(document).ready(function () {
     $("#main-nav").toggleClass("active");
     $("body").toggleClass("active-menu");
     $(".container").toggleClass("active-menu");
-    $(".main-nav .menus").toggleClass('show');
-    $("header").toggleClass('show');
+    $(".main-nav .menus").toggleClass("show");
+    $("header").toggleClass("show");
   });
 
-  $(".for-mobile .hamburger").click(function(e) {
-		$(this).toggleClass("active");
+  $(".for-mobile .hamburger").click(function (e) {
+    $(this).toggleClass("active");
     $("#main-nav").toggleClass("active");
-		$("body").toggleClass("active-menu");
-	});
-
-  $('.harmonic div').on('click', function() {
-    $('.harmonic div').removeClass('active');
-    $(this).addClass('active');
+    $("body").toggleClass("active-menu");
   });
+
+  if ($(window).width() <= 425) {
+    $(".harmonic").bxSlider({
+      touchEnabled: true,
+    });
+  } else {
+    $(".harmonic div").on("click", function () {
+      $(".harmonic div").removeClass("active");
+      $(this).addClass("active");
+    });
+  }
+
+  $('.gallery-wrapper').magnificPopup({type:'image', delegate: 'a.image-link', gallery:{enabled:true}});
 });
