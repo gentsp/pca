10c91750ea9b5ba732827863079d79b42d9f5eb6
Gent Spahiu <gentsp@moosend.com>
2021-08-23 23:40:26 +0200

    Add scrollmagic and parallax

--- js/main.js
+++ js/main.js
@@ -25,5 +25,31 @@ $(document).ready(function () {
     });
   }
 
-  $('.gallery-wrapper').magnificPopup({type:'image', delegate: 'a.image-link', gallery:{enabled:true}});
+  if ($(".gallery-wrapper").length)
+    $(".gallery-wrapper").magnificPopup({
+      type: "image",
+      delegate: "a.image-link",
+      gallery: { enabled: true },
+    });
+
+  var controller = new ScrollMagic.Controller({
+    globalSceneOptions: {
+      triggerHook: "onLeave",
+      duration: "200%", // this works just fine with duration 0 as well
+      // However with large numbers (>20) of pinned sections display errors can occur so every section should be unpinned once it's covered by the next section.
+      // Normally 100% would work for this, but here 200% is used, as Panel 3 is shown for more than 100% of scrollheight due to the pause.
+    },
+  });
+
+  // get all slides
+  var slides = document.querySelectorAll("section.panel");
+
+  // create scene for every slide
+  for (var i = 0; i < slides.length; i++) {
+    new ScrollMagic.Scene({
+      triggerElement: slides[i],
+    })
+      .setPin(slides[i], { pushFollowers: false })
+      .addTo(controller);
+  }
 });
