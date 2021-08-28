80df81b0c7324226e9bce5a8af84aa3c18a924dc
Gent Spahiu <gentsp@moosend.com>
2021-08-28 14:12:08 +0200

    Add new changes. Parallax properly

--- js/main.js
+++ js/main.js
@@ -32,24 +32,40 @@ $(document).ready(function () {
       gallery: { enabled: true },
     });
 
-  var controller = new ScrollMagic.Controller({
-    globalSceneOptions: {
-      triggerHook: "onLeave",
-      duration: "200%", // this works just fine with duration 0 as well
-      // However with large numbers (>20) of pinned sections display errors can occur so every section should be unpinned once it's covered by the next section.
-      // Normally 100% would work for this, but here 200% is used, as Panel 3 is shown for more than 100% of scrollheight due to the pause.
-    },
-  });
+  // var controller = new ScrollMagic.Controller({
+  //   globalSceneOptions: {
+  //     triggerHook: "onLeave",
+  //     duration: "200%", // this works just fine with duration 0 as well
+  //     // However with large numbers (>20) of pinned sections display errors can occur so every section should be unpinned once it's covered by the next section.
+  //     // Normally 100% would work for this, but here 200% is used, as Panel 3 is shown for more than 100% of scrollheight due to the pause.
+  //   },
+  // });
 
-  // get all slides
-  var slides = document.querySelectorAll("section.panel");
+  // // get all slides
+  // var slides = document.querySelectorAll("section.panel");
 
-  // create scene for every slide
-  for (var i = 0; i < slides.length; i++) {
-    new ScrollMagic.Scene({
-      triggerElement: slides[i],
-    })
-      .setPin(slides[i], { pushFollowers: false })
-      .addTo(controller);
+  // // create scene for every slide
+  // for (var i = 0; i < slides.length; i++) {
+  //   new ScrollMagic.Scene({
+  //     triggerElement: slides[i],
+  //   })
+  //     .setPin(slides[i], { pushFollowers: false })
+  //     .addTo(controller);
+  // }
+
+  var controller = new ScrollMagic.Controller({globalSceneOptions: {triggerHook: "onEnter", duration: "200%"}});
+
+  new ScrollMagic.Scene({triggerElement: "section.panel"})
+					.setTween("section.panel .parallaxChildren", {y: "80%", ease: Linear.easeNone})
+					.addTo(controller);
+
+  $('.side-info .scrollElem').on('click', function() {
+    if($(this).hasClass('bottom')) {
+    window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
+    $(this).removeClass('bottom').addClass('top');
+  } else {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+    $(this).removeClass('top').addClass('bottom');
   }
+  });
 });
